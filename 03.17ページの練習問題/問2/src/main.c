504af8b048d8aca6efdf114a1cc13637929810c3
std::Enkū <38544465+StdEnku@users.noreply.github.com>
2025-04-09 22:45:40 +0900

    ずれを修正

--- 03.17ページの練習問題/問2/src/main.c
+++ 03.17ページの練習問題/問2/src/main.c
@@ -7,10 +7,11 @@ int main(void)
 	const int HOUR_IN_DAY = 24;
 	const int DAY_IN_YEAR = 365;
 	
-	const int SECOND_IN_YEAR = SECOND_IN_MINUTE *
-							   MINUTE_IN_HOUR *
-							   HOUR_IN_DAY *
-							   DAY_IN_YEAR;
+	const int SECOND_IN_YEAR = 
+		SECOND_IN_MINUTE *
+		MINUTE_IN_HOUR *
+		HOUR_IN_DAY *
+		DAY_IN_YEAR;
 	
 	printf("一年間の秒数は%d秒\n", SECOND_IN_YEAR);
 	
